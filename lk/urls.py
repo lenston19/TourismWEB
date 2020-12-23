c9ea98043181be8e61e1d628075806e1c185b3bf
lenston <lenston190900@gmail.com>
2020-12-23 15:00:28 +0300

    Change lk

--- lk/urls.py
+++ lk/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
-from .views import SignUp
+from .views import lk
 
 urlpatterns = [
-    url('lk', views.lk, name='lk'),
+    url('lk', lk.as_view(), name='lk'),
     url('registr', views.SignUp, name='registr'),
 ]
--- lk/views.py
+++ lk/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponseRedirect
+
+from django.views.generic.list import ListView
 from django.shortcuts import render
-from django.urls import reverse_lazy
-
+from detail.models import Hotel
+from detail.views import SearchResultsView
 from lk.forms import RegistrationForm
-# Create your views here.
 
-def lk(request):
-    return render(request, 'lk/lk.html')
+# def lk(request):
+#     return render(request, 'lk/lk.html')
 
 def SignUp(request):
     if request.method == 'POST':
@@ -17,3 +17,16 @@ def SignUp(request):
     else:
         form = RegistrationForm()
         return render(request, 'lk/registr.html', {'form': form})
+
+class lk(ListView):
+    model = Hotel
+    template_name = 'lk/lk.html'
+    paginate_by = 3
+
+    def get_queryset(self):  # новый
+        HotelName = self.kwargs.get("HotelName")
+        if HotelName:
+            object_list = Hotel.objects.filter(HotelName__icontains=HotelName)
+        else:
+            object_list = Hotel.objects.all()
+        return object_list
